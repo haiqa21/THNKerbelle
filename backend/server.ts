802faf9772b7c33301f7ecbd053419c009a4abda
urGitHubUsrName <z5590411@ad.unsw.ed.au>
2026-03-14 14:44:30 +1100

    all the stuff

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,10 @@
-import express from 'express'
+import express, { json, Request, Response } from 'express';
 import cors from 'cors'
+import morgan from 'morgan';
+import YAML from 'yaml';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
 // import authRoutes  from './routes/auth.js'
 
 const app = express()
@@ -7,7 +12,7 @@ const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(cors())                    // allow requests from your React Native app
-app.use(express.json())            // parse JSON request bodies
+app.use(json())            // parse JSON request bodies
 app.use(morgan('dev'));
 // Routes
 // app.use('/auth',  authRoutes)
@@ -21,16 +26,3 @@ app.listen(PORT, () => {
   console.log(`   app.db auto-created next to server.js if it didn't exist`)
 })
 
-
-app.post('/v1/admin/auth/register', (req: Request, res: Response) => {
-  const { email, password, nameFirst, nameLast } = req.body;
-
-  try {
-    const result = adminAuthRegister(email, password, nameFirst, nameLast);
-    const sessionId = createSession(result.userId);
-    return res.json({ session: sessionId });
-  } catch (err) {
-    const message = (err as Error).message;
-    return res.status(400).json({ error: message });
-  }
-});
